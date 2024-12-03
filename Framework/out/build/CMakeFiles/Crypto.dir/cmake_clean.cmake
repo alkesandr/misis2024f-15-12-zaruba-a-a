59bcdb1de94d8ef1de903435ce71bd02679482eb
aleksandrzaruba <111224067+alkesandr@users.noreply.github.com>
2024-12-03 20:09:19 +0300

    Add files via upload

---
+++ Framework/out/build/CMakeFiles/Crypto.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Crypto.dir/main.cpp.o"
+  "CMakeFiles/Crypto.dir/main.cpp.o.d"
+  "Crypto"
+  "Crypto.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Crypto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
